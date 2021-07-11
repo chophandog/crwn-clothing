942902a2a9686f25adc090f1290bae5b4084550d
Your Name <chophandog@gmail.com>
2021-07-11 11:44:05 +0700

    adding header component, collection preview, collection item, shoppage  and link to home, shop, contact

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
-import "./App.css";
+import { Switch, Route } from "react-router-dom";
+
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
+
+import "./App.css";
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,20 +1,24 @@
 import React from "react";
+import { withRouter } from "react-router-dom";
 
 import "./menu-item.styles.scss";
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`menu-item ${size}`}>
-
-    <div style={{
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+  <div
+    className={`menu-item ${size}`}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
+  >
+    <div
+      style={{
         backgroundImage: `url(${imageUrl})`,
       }}
-      className= "background-image"
-       />
+      className="background-image"
+    />
     <div className="content">
-      <h1 className="title">{title}</h1>
+      <h1 className="title">{title.toUpperCase()}</h1>
       <span className="subtitle">SHOP NOW</span>
     </div>
   </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
